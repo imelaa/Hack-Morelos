464cedd4a7358270791cd047766c5ebaf2c05066
ela <imela2811@gmail.com>
2024-05-17 00:56:12 -0600

    idk

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
 import Image from "next/image";
-import navbar from "./components/navbar";
 import Sidebar from "./components/sidebar";
+import Navbar from "./components/navbar";
+
+import Footer from "./components/footer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-  
-      <Sidebar/>
-        <h1>hello world</h1>
+    <Navbar/>
+    <Sidebar/>
+    {/* <Content/> */}
+    <Footer/>
+      
+        
+
 
 
     </main>
