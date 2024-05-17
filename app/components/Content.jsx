6cf7b8a6ac3fef586e2d9a95f0a78e614ef22584
ela <imela2811@gmail.com>
2024-05-17 01:49:22 -0600

    Inicio

---
+++ app/components/Content.jsx
@@ -0,0 +1,15 @@
+
+
+const Content = () => {
+    return (
+        <>
+        <h1>Content</h1>
+        
+        </>
+
+
+
+    )
+
+}
+export default Content;
---
+++ app/components/Footer.jsx
@@ -0,0 +1,15 @@
+
+
+const Footer = () => {
+    return (
+        <>
+        <h1>Footer</h1>
+        
+        </>
+
+
+
+    )
+
+}
+export default Footer;
