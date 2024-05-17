29bfbcc1283928ad40739413c47358849747e242
Josue Santamaria <josuexsanta@gmail.com>
2024-05-17 06:27:16 -0600

    funciona el click en las cards

--- app/components/Cards/Card.jsx
+++ app/components/Cards/Card.jsx
@@ -1,24 +1,25 @@
 // components/FeaturedCard.js
+"use client";
+
 import Image from "next/image";
+import React from "react";
 
-const elemento = {
-  imageUrl: "/images/placeholder.jpg",
-  textoDialecto: "Niyolpaki ipampa nimitsixmati",
-  textoIngles: "Hi, how are you?",
-};
+export default function Card(props) {
+  const handleClick = () => {
+    console.log("Aqui iria el audio");
+  };
 
-export default function Card(elemento) {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-4 w-max">
+    <div className="bg-white shadow-lg rounded-lg p-4 w-max" onClick={handleClick}>
       <Image
-        src={elemento.imageUrl}
+        src={props.imageUrl}
         alt="Placeholder"
         width={300}
         height={200}
         className="rounded-lg"
       />
-      <p className="text-color-6">{elemento.textoDialecto}</p>
-      <p className="text-color-6">{elemento.textoIngles}</p>
+      <p className="text-color-6">{props.textoDialecto}</p>
+      <p className="text-color-6">{props.textoIngles}</p>
     </div>
   );
 }
