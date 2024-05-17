6a27d74073f429f8de327a6ff8a75283b1b0ce07
ela <imela2811@gmail.com>
2024-05-16 23:26:58 -0600

    se empieza

---
+++ app/components/sidebar.js
@@ -0,0 +1,11 @@
+
+const Sidebar = () => {
+  return (
+    <div className="bg-gray-800 text-white w-64 min-h-screen p-4">
+      <h2 className="text-xl font-semibold">Sidebar</h2>
+      {/* Agrega aquí tus elementos de la barra lateral */}
+    </div>
+  );
+};
+
+export default Sidebar;
