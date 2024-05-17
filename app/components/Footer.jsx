01dfeb66aaf9fc9d8c0a3e88406cc17591509833
ieshu666 <alvarez.ieshuah@gmail.com>
2024-05-17 09:55:49 -0600

    Footer con arreglos de diseño

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bottom-0 bg-gray-800 text-white w-full">
-      <div className="container mx-auto text-center">
+    <footer className="bottom-0 bg-white text-white w-full ">
+      <div className="container mx-auto text-center text-stone-500">
         <p>&copy; {new Date().getFullYear()} Culturas conectadas. Todos los derechos reservados.</p>
-        <ul className="flex justify-center space-x-4 mt-2">
+        <ul className="flex justify-center space-x-4 mt-2 mb-2">
           <li><a href="/about" className="hover:underline">Sobre nosotros</a></li>
           <li><a href="/contact" className="hover:underline">Contacto</a></li>
           <li><a href="/privacy" className="hover:underline">Política de privacidad</a></li>
