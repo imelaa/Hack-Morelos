06dc7de33f76066b1ed7dd7b94da13d5e8470af4
ela <imela2811@gmail.com>
2024-05-17 05:04:48 -0600

    ready ahora si

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,28 +1,54 @@
-// components/Sidebar.js
+import React from 'react';
+
+
+
+// const boton ={
+//   imgUrl: "/assets/test.jpg"
+//   texto: 
+// }
 
 const Sidebar = () => {
   return (
-    <div className="h-screen w-64 bg-gray-800 text-white fixed left-0">
-      <div className="py-4 px-6">
-        <h1 className="text-2xl font-semibold">My Sidebar</h1>
-      </div>
+
+    
+    <div className="fixed top-0 left-0 h-full w-64 bg-gray-760 text-white p-4 backdrop-blur rounded-lg border border-blue transition duration-300">
+      <div className="py-4 px-6 ">
+  
+        <img src="../assets/test.jpg" alt="jgf" className="h-12 w-12 rounded-full" />
+        </div>
       <nav className="mt-6">
         <ul>
-          <li className="px-4 py-2 hover:bg-gray-700">
-            <a href="/archivo1.txt" target="_blank" rel="noopener noreferrer">Archivo 1</a>
+          <div>
+            <a href="../../Renders/A6-Social" className="sidebar-link btn block text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300">Social</a>
+          </div>
+          <li>
+            <a href="../../Renders/A7-Trabajo" className="sidebar-link block text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300">Trabajo</a>
           </li>
-          <li className="px-4 py-2 hover:bg-gray-700">
-            <a href="/archivo2.txt" target="_blank" rel="noopener noreferrer">Archivo 2</a>
+          <li>
+            <a href="../../Renders/A1-Comida" className="sidebar-link block text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300">Comida</a>
           </li>
-          <li className="px-4 py-2 hover:bg-gray-700">
-            <a href="/archivo3.txt" target="_blank" rel="noopener noreferrer">Archivo 3</a>
+          <li>
+            <a href="../../Renders/A4-Escuela" className="sidebar-link block text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300">Escuela</a>
+          </li>
+          <li>
+            <a href="../../Renders/A2-Numeros" className="sidebar-link block text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300">Numeros</a>
+          </li>
+          <li>
+            <a href="../../Renders/A3-Direccion" className="sidebar-link block text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300">Direccion</a>
+          </li>
+          <li>
+            <a href="../../Renders/A5-Preposiciones" className="sidebar-link block text-white py-2 px-4 rounded hover:bg-gray-600 transition duration-300">Preposiciones</a>
           </li>
         </ul>
       </nav>
+
+
+      <div className="fixed top-0 left-0 h-full w-64 bg-gray-760 text-white p-4 backdrop-blur rounded-lg border border-blue transition duration-300">
+      
     </div>
+    </div>
+    
   );
 }
+
 export default Sidebar;
-// components/Sidebar.js
-
-
--- app/page.js
+++ app/page.js
@@ -1,22 +1,14 @@
 import Image from "next/image";
-
-import Sidebar from "./components/sidebar";
-import Navbar from "./components/navbar";
-
-import Footer from "./components/footer";
-
-
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Content from "./components/Content";
-
-import Card from "./components/Cards/Card";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Navbar/>
-      <Sidebar/>
-      <Content/>
-      <Footer/>
+      <Sidebar />
+
     </main>
   );
 }
