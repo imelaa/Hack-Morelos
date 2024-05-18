7eadadc1898c0a656001460c2f1f1fe49d1fe976
ela <imela2811@gmail.com>
2024-05-18 07:52:21 -0600

    Ready para empezar a hacer los merge

---
+++ app/Renders/RecoImg/ContentR.jsx
@@ -0,0 +1,19 @@
+
+
+const Content = () => {
+    return (
+        <div className="bg-color-7 rounded-lg p-4">
+
+            <div>
+                insertar imagen
+            </div>
+
+            <div>
+                Analizar imagen
+            </div>
+        </div>
+    );
+}
+
+export default Content;
+
