f12dfb5f2c3f89f9142bbad0feb8045aca6beb20
ela <imela2811@gmail.com>
2024-05-17 18:57:47 -0600

    fixxdg

--- app/Renders/comunidad/page.jsx
+++ app/Renders/comunidad/page.jsx
@@ -1,3 +1,4 @@
+import Navbar from "../../components/Navbar";
 import Comunidad from "./Comunidad";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-row items-center bg-color-8 flex-auto">
         <div className="flex">
             <Comunidad/>
+            <Navbar />
         </div>
     </main>
   );
