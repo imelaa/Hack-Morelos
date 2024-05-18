b4084696de3ccad7f515f1478ea50b2ef3450e0a
ela <imela2811@gmail.com>
2024-05-18 09:48:10 -0600

    arreglado comunidad

--- app/Renders/comunidad/page.jsx
+++ app/Renders/comunidad/page.jsx
@@ -1,7 +1,6 @@
 "use client";
 import Navbar from "../../components/Navbar";
 import MultiStepForm from "./Comunidad";
-import Navbar from "../../components/Navbar";
 export default function Home() {
   return (
     <main className="flex w-screen min-h-screen flex-row  bg-color-8 flex-auto">
